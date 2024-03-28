0cacee9d8934e282155d0bff612a413542f8a223
Ephra <ephygtz@gmail.com>
2024-03-28 16:13:51 +0300

    Java: binary search recursively

--- JAVA/LinearBinarySearch.java
+++ JAVA/LinearBinarySearch.java
@@ -2,13 +2,16 @@ public class LinearBinarySearch {
 
     public static void main(String[] args) {
         int[] nums = {1,3,5,7,9,11,13,15,17,19,21};
-        int target = 11;
+        int target = 9;
 
         //using linearSearch(Big0(n)
         int result2 = linearSearch(nums, target);
 
+        //using binarySearch(Big0(Logn)
         int result = binarySearch(nums, target);
+
         //using binarySearchRecursively(Big0(Logn)
+        int result1 = binarySearchRecursive(nums, target, 0, nums.length-1);
 
         if(result != -1){
             System.out.println("Element found at index : " + result);
@@ -43,6 +46,20 @@ public class LinearBinarySearch {
                 right = mid - 1;
             }
         }
+        return -1;
+    }
+
+    public static int binarySearchRecursive(int[] nums, int target, int left, int right){
+        int mid = (left + right) / 2;
+
+        if(nums[mid] == target){
+            return mid;
+        } else if(nums[mid] < target){
+            binarySearchRecursive(nums, target, mid + 1, right);
+        } else {
+            binarySearchRecursive(nums, target, left, mid - 1);
+        }
+
         return -1;
     }
 }
