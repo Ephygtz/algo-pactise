51ffeaa9bff64fa8ab59ffc462ca9713f7525428
Ephra <ephygtz@gmail.com>
2024-03-28 15:48:35 +0300

    Java: binary search

--- JAVA/LinearBinarySearch.java
+++ JAVA/LinearBinarySearch.java
@@ -2,10 +2,13 @@ public class LinearBinarySearch {
 
     public static void main(String[] args) {
         int[] nums = {1,3,5,7,9,11,13,15,17,19,21};
-        int target = 19;
+        int target = 11;
 
         //using linearSearch(Big0(n)
-        int result = linearSearch(nums, target);
+        int result2 = linearSearch(nums, target);
+
+        int result = binarySearch(nums, target);
+        //using binarySearchRecursively(Big0(Logn)
 
         if(result != -1){
             System.out.println("Element found at index : " + result);
@@ -18,6 +21,26 @@ public class LinearBinarySearch {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i]  == target){
                 return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int[] nums, int target) {
+        //define start and end
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while(left <= right){
+            //find mid point
+            int mid = (left + right) / 2;
+
+            if(nums[mid] == target){
+                return mid;
+            } else if(nums[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return -1;
